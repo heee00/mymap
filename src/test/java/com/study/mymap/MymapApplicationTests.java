06cfd9048a753b3d8ea3ef7ce3320d22bad62e73
DESKTOP-K944R1E\jesic <hwheo@netcomsolution.co.kr>
2023-09-02 14:16:26 +0900

    프로젝트를 시작한다

---
+++ src/test/java/com/study/mymap/MymapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
